77b4d028c48c256668af4cc63aa82054679c8033
Sumit41976 <30293943+Sumit41976@users.noreply.github.com>
2024-02-13 00:53:13 +0530

    hello

---
+++ GenericSeleniumFramework_Copy/src/main/java/pages/OrangeHRMLoginPage.java
@@ -0,0 +1,39 @@
+package pages;
+
+
+import org.openqa.selenium.By;
+import enums.WaitStrategy;
+
+public final class OrangeHRMLoginPage extends BasePage {
+
+    /*NOTES
+     * 1. Assertions should be always created at the test page level and not page objects.
+     * 2. All of the page methods needs to have some return type for method chaining and other stuff.
+     */
+
+    //BasePage bp = new BasePage();
+    private final By textboxUsername = By.name("username");
+    private final By textboxPassword = By.name("password");
+    private final By buttonLogin = By.xpath("//button[@type='submit']");
+
+    public OrangeHRMLoginPage enterUserName(String username){
+        sendKeys(textboxUsername, username, WaitStrategy.PRESENCE);
+        return this;
+    }
+
+    public OrangeHRMLoginPage enterPassword(String password){
+        sendKeys(textboxPassword, password, WaitStrategy.PRESENCE);
+        return this;
+    }
+
+
+    public OrangeHRMHomePage clickLoginButton(){
+        click(buttonLogin, WaitStrategy.CLICKABLE);
+        return new OrangeHRMHomePage();
+    }
+
+    public String getTitle( ){
+        return getPageTitle();
+    }
+
+}
